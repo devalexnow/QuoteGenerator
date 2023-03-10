0dd22a72d763850a227f5c4c7017c6be596f276a
devalexnow <a.nowak.dev@gmail.com>
2023-03-10 19:17:58 +0100

    pliki

---
+++ quotes.js
@@ -0,0 +1,42 @@
+const localQuotes = [
+    {
+        text: "Można. Jeszcze jak!",
+        author: "Jan Paweł II",
+    },
+    {
+        text: "Tak jak Pan Jezus powiedział",
+        author: "Jan Paweł II",
+    },
+    {
+        text: "Przeniść do innej parafii",
+        author: null,
+    },
+    {
+        text: "Wiecie co z nim zrobić!",
+        author: "Peja",
+    },
+    {
+        text: "Wszystkie dzieci nasze są",
+        author: "Majka Jeżowska",
+    },
+    {
+        text: "Jakby się kto pytał - Frańciszkańska 3",
+        author: "Jan Paweł II",
+    },
+    {
+        text: "Po maturze chodziliśmy na kremówki",
+        author: "Jan Paweł II",
+    },
+    {
+        text: "...i odnowi oblicze Ziemi. Tej Ziemi",
+        author: "Jan Paweł II",
+    },
+    {
+        text: "Swoją barkę pozostawiam na brzegu",
+        author: "Jan Paweł II",
+    },
+    {
+        text: "...ja bym z takim Papieżem nie wytrzymał",
+        author: "Jan Paweł II",
+    }
+]
--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("sjkdjsd");
+const quoteContainer = document.getElementById('guote-container');
+const authorText = document.getElementById("author");
+const quotesText = document.getElementById("quotes");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+
+// Show new quote
+function newQuote() {
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+// Check if author is empty
+    if (!quote.author) {
+        authorText.textContent = "autor nieznany";
+    } else {
+        authorText.textContent = quote.author;
+    }
+// Check if quote is too long 
+    if (quote.text.length > 30) {
+        quotesText.classList.add('long-quote');
+    } else {
+        quotesText.classList.remove('long-quote');
+    }
+    quotesText.textContent = quote.text;
+}
+
+//Tweet a quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quotesText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, `_blank`);
+}
+
+//Event listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+
+newQuote()
